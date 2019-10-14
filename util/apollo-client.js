c28055b4676789dd5eaf9e8b65a9b16bef49e935
= <=>
2019-10-15 08:40:02 +1100

    Add ssrMode to Apollo Client config

--- util/apollo-client.js
+++ util/apollo-client.js
@@ -1,23 +1,24 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { createHttpLink } from 'apollo-link-http';
-import fetch from 'node-fetch';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import fetch from "node-fetch";
 
 const client = new ApolloClient({
   // Provide required constructor fields
   cache: new InMemoryCache(),
   link: createHttpLink({
     fetch,
-    uri: 'https://api.graphql.jobs/'
+    uri: "https://api.graphql.jobs/"
   }),
-
+  // Fetch each query result once
+  ssrMode: true,
   // Provide some optional constructor fields
-  name: 'react-web-client',
-  version: '1.3',
+  name: "react-web-client",
+  version: "1.3",
   queryDeduplication: false,
   defaultOptions: {
     watchQuery: {
-      fetchPolicy: 'cache-and-network'
+      fetchPolicy: "cache-and-network"
     }
   }
 });
