46deddf4f5529c4ff11e0e47ce9f0321f24a3ac0
Tom Nagle <tomanagle@gmail.com>
2019-10-14 21:28:58 +1100

    Add custom _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import App from 'next/app';
+import ApolloClient from 'apollo-client';
+import client from '../util/apollo-client';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+    return (
+      <ApolloClient>
+        <Component {...pageProps} />
+      </ApolloClient>
+    );
   }
 }
 
