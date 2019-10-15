2f50700108bb6babc230fc47fa4a31375d0165e1
= <=>
2019-10-15 13:08:59 +1100

    Add linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,27 @@
-import React from 'react';
 import App from 'next/app';
 import { ApolloProvider } from '@apollo/react-hooks';
-import client from '../util/apollo-client';
+
+import withData from '../util/apollo-client';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
     return (
-      <ApolloProvider client={client}>
+      <ApolloProvider client={apollo}>
         <Component {...pageProps} />
       </ApolloProvider>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
